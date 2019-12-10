eed453f89541d2ed19c82f8070b34e208e682782
astillo <clomes.alex@gmail.com>
2019-12-10 08:52:59 -0600

    fixed algos

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,17 +2,16 @@
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+        smallest_index = i
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
         for j in range(i+1, len(arr)):
-            if arr[j] < smallest_index:
+            if arr[smallest_index] > arr[j]:
                 smallest_index = j
         # TO-DO: swap
         if smallest_index != i:
             arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
-
+            
 
 
     return arr
@@ -22,9 +21,11 @@ def selection_sort( arr ):
 def bubble_sort( arr ):
     isSorted = False
     while not isSorted:
+        isSorted=True
         for i in range(0, len(arr)-1):
             if arr[i] > arr[i+1]:
                 arr[i], arr[i+1] = arr[i+1], arr[i]
+                isSorted = False
     return arr
 
 
